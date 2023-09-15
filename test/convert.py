ad16917ebaf1f26cb87fb4e222b4fde907822e63
bkane2 <Ben.kane@me.com>
2023-09-15 15:36:07 -0400

    Updated documentation

--- test/convert.py
+++ test/convert.py
@@ -7,7 +7,8 @@ from ulf2english import ulf2english
 # ulf = ['John', [['pres', 'prog'], 'go.v', ['to.p', ['the.d', 'store.n']]]]
 # ulf = ['John', [['prog', 'go.v'], 'there.pro']]
 # ulf = [['set-of', 'Testone.name', 'Testtwo.name', 'Testthree.name'], [['pres', 'be.v'], ['=', ['a.d', ['test.a', 'ulf.n']]]]]
-ulf = ['John', ['pres', ['pasv', 'confuse.v']]]
+# ulf = ['John', ['pres', ['pasv', 'confuse.v']]]
+ulf = ['this.pro', [['pres', 'be.v'], ['=', ['a.d', ['test.n', 'ulf.n']]]]]
 # ulf = [['what.pro', [['pres', 'be.v'], ['=', ['that.d', 'thing.n']]]], '?']
 
 print(ulf2english.convert(ulf, verbose=True))
